e21388b26a48ae3ebca2f52f0964739ca5a43494
Julien Cayzac | SSED <julien.cayzac@rakuten.com>
2017-10-20 10:11:57 +0900

    🐣 initial

---
+++ Team4/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Team4
+//
+//  Created by Julien Cayzac on 10/20/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
