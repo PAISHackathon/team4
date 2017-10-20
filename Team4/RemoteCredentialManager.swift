bde88e8f6220304b1877f56ce6f3ae6b7b0bf45c
Julien Cayzac | SSED <julien.cayzac@rakuten.com>
2017-10-20 12:27:00 +0900

    Connection management

--- Team4/RemoteCredentialManager.swift
+++ Team4/RemoteCredentialManager.swift
@@ -20,6 +20,7 @@ class RemoteCredentialManager :
     private var advertiser : MCNearbyServiceAdvertiser!
     private var browser : MCNearbyServiceBrowser!
     private var session : MCSession!
+    
     private var isBroadcasting = false
     private var isListening = false
 
@@ -39,71 +40,80 @@ class RemoteCredentialManager :
     }
 
     // MARK: Broadcasting
+    // ------------------
+    //
+    // Used by devices that have credentials to share.
+    //
 
     // Should be called when the app is foreground and has credentials
     func startBroadcasting() {
-        if (!isBroadcasting) {
-            advertiser.startAdvertisingPeer()
-            isBroadcasting = true
-        }
+        advertiser.startAdvertisingPeer()
+        isBroadcasting = true
     }
 
     // Should be called when the app goes to background
     func stopBroadcasting() {
-        if (isBroadcasting) {
-            advertiser.stopAdvertisingPeer()
-            isBroadcasting = false
-        }
+        advertiser.stopAdvertisingPeer()
+        isBroadcasting = false
     }
 
     // MARK: Listening
+    // ---------------
+    //
+    // Used by devices that don't have any credential, when a user wants to log in
 
     // Should be called by the login form when it's shown
     func startListening() {
-        if (!isListening) {
-            browser.startBrowsingForPeers()
-            isListening = true
-        }
+        browser.startBrowsingForPeers()
+        isListening = true
     }
 
     // Should be called by the login form when it's dismissed
     func stopListening() {
-        if (isListening) {
-            browser.stopBrowsingForPeers()
-            isListening = false
-        }
+        browser.stopBrowsingForPeers()
+        isListening = false
     }
 
     // MARK: MCNearbyServiceAdvertiserDelegate
+
     func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
         print("Did not start advertising: \(error.localizedDescription)")
     }
 
+    // Grant/Deny peer credential requests
     func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
-        let alert = UIAlertController(title: nil,
-                                      message: "[\(peerID.displayName)] would like to access your credentials",
-                                      preferredStyle: .alert)
+        print("Received invitation from [\(peerID.displayName)] peer")
 
-        alert.addAction(UIAlertAction(title: "Deny",
-                                      style: .cancel,
-                                      handler: { _ in invitationHandler(false, nil) }))
+        if isBroadcasting {
+            let alert = UIAlertController(title: nil,
+                                          message: "[\(peerID.displayName)] would like to access your credentials",
+                                          preferredStyle: .alert)
 
-        alert.addAction(UIAlertAction(title: "Grant",
-                                      style: .default,
-                                      handler: { _ in invitationHandler(true, self.session) }))
+            alert.addAction(UIAlertAction(title: "Deny",
+                                          style: .cancel,
+                                          handler: { _ in invitationHandler(false, nil) }))
 
-        // Dirty!
-        if let appDelegate = UIApplication.shared.delegate as? AppDelegate,
-           let parent = appDelegate.window?.rootViewController?.presentingViewController {
-            parent.show(alert, sender: nil)
+            alert.addAction(UIAlertAction(title: "Grant",
+                                          style: .default,
+                                          handler: { _ in invitationHandler(true, self.session) }))
+
+            // Dirty!
+            if let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+               let parent = appDelegate.window?.rootViewController?.presentingViewController {
+                parent.show(alert, sender: nil)
+            }
         }
     }
 
     // MARK: MCNearbyServiceBrowserDelegate
+
+    //
     func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
-        // Found a server
-        if (isListening) {
-            stopListening()
+        print("Found [\(peerID.displayName)] peer")
+
+        if isListening {
+            // Invite server to join the session. Note we may invite multiple servers, but it's ok
+            // we'll just take the credentials from the first that grants us access.
             browser.invitePeer(peerID, to: session, withContext: nil, timeout: 30.0)
         }
     }
